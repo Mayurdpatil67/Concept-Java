5093e7ea33b7efd17c82dbf7bc2b13ba32381b9b
Mayurdpatil67 <mayurpatil.work@gmail.com>
2024-05-04 19:16:58 +0530

    concept of final

---
+++ Final2.java
@@ -0,0 +1,21 @@
+final class Demo 
+{
+   public int A;
+   public int B;
+}
+
+/*
+class Hello  extends Demo   //Cannot inherit from final class Demo
+{
+
+}
+*/
+
+class Final2
+{
+    public  static void main (String Arg[])
+    {
+
+    }
+
+}
---
+++ Final3.java
@@ -0,0 +1,32 @@
+class Demo
+{
+    public  void fun()
+    {
+
+    }
+    public final void gun()
+    {
+
+    }
+}
+
+class Hello extends Demo
+{
+    public void fun ()
+    {
+
+    }
+ /* public void gun()  // gun() in Hello cannot override gun() in Demo because of final method 
+    {
+
+    }*/
+}
+
+class Final3
+{
+    public  static void main (String Arg[])
+    {
+
+    }
+
+}
